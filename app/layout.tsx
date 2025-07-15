5586a14ae5aa4a1fdc8f534cdefb58a15323b550
siripol <siripol.th@ku.th>
2025-07-15 11:43:47 +0700

    feat: add icons and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,12 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description: "A platform for developers to share their knowledge",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
