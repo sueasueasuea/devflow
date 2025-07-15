e924eeeab5d2f799f564c50714553a68f019e9a1
siripol <siripol.th@ku.th>
2025-07-15 11:00:16 +0700

    feat: font, design system, eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-      <h1 className="text-5xl font-bold">DevFlow</h1>
+    <>
+      <h2 className="font-space-grotesk">DevOverFlow</h2>;
+      <h1 className="pt-10 font-bold text-primary-500 text-5xl">DevFlow</h1>;
+      <h1 className="font-inter text-5xl">DevFlow right now</h1>;
+    </>
   );
 }
